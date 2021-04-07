06b22e798da579c25b4d92b4ad937ddd7534f7ae
jbwang1997 <709370615@qq.com>
2021-04-07 21:55:36 +0800

    change the obb dataset path

--- configs/obb/_base_/datasets/dior_poly.py
+++ configs/obb/_base_/datasets/dior_poly.py
@@ -1,5 +1,5 @@
 dataset_type = 'DIORDataset'
-data_root = '/data2/wjb/dataset/DIOR/'
+data_root = '/path/to/DIOR/dataset/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
--- configs/obb/_base_/datasets/hrsc_obb.py
+++ configs/obb/_base_/datasets/hrsc_obb.py
@@ -1,5 +1,5 @@
 dataset_type = 'HRSCDataset'
-data_root = '/disk2/xiexingxing/wjb/HRSC2016/'
+data_root = '/path/to/HRSC/dataset/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
