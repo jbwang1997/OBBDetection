e4f82c0e2fd459da93c5d2cbdf15c8a5c3cc87c0
jbwang1997 <jbwang1997@gmail.com>
2021-08-06 11:26:01 +0800

    rm useless files

--- .dev_scripts/linter.sh
+++
@@ -1,3 +0,0 @@
-yapf -r -i --style .style.yapf mmdet/ configs/ tests/ tools/
-isort -rc mmdet/ configs/ tests/ tools/
-flake8 .
--- check_config.py
+++
@@ -1,2 +0,0 @@
-from mmcv import Config
-cfg = Config.fromfile('configs/obb/roi_transformer_r50_fpn_1x_dota.py')
