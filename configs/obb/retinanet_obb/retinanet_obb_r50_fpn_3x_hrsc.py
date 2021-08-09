a31319c6da9af9a79e2a3d2a4b6201b961dffea9
jbwang1997 <jbwang1997@gmail.com>
2021-08-09 10:41:27 +0800

    Fix hrsc support

--- configs/obb/retinanet_obb/retinanet_obb_r50_fpn_3x_hrsc.py
+++ configs/obb/retinanet_obb/retinanet_obb_r50_fpn_3x_hrsc.py
@@ -1,87 +1,76 @@
-_base_ = './retinanet_obb_r50_fpn_1x_dota10.py'
-
-# 3x schedules
-lr_config = dict(step=[24, 33])
-total_epochs = 36
-
-# HRSC2016 dataset cfg
-dataset_type = 'HRSCDataset'
-data_root = 'data/hrsc/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadOBBAnnotations', with_bbox=True,
-         with_label=True, with_poly_as_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='OBBRandomFlip', h_flip_ratio=0.5, v_flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='RandomOBBRotate', rotate_after_flip=True,
-         angles=(0, 0), vert_rate=0.5),
-    dict(type='Pad', size_divisor=32),
-    dict(type='Mask2OBB', obb_type='obb'),
-    dict(type='OBBDefaultFormatBundle'),
-    dict(type='OBBCollect', keys=['img', 'gt_bboxes', 'gt_obboxes', 'gt_labels'])
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipRotateAug',
-        img_scale=[(1333, 800)],
-        h_flip=False,
-        v_flip=False,
-        rotate=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='OBBRandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='RandomOBBRotate', rotate_after_flip=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='OBBCollect', keys=['img']),
-        ])
+_base_ = [
+    '../_base_/datasets/hrsc.py',
+    '../_base_/schedules/schedule_3x.py',
+    '../../_base_/default_runtime.py'
 ]
 
-# does evaluation while training
-# uncomments it  when you need evaluate every epoch
-# data = dict(
-    # samples_per_gpu=2,
-    # workers_per_gpu=4,
-    # train=dict(
-        # type=dataset_type,
-        # imgset=data_root + 'ImageSets/train.txt',
-        # ann_file=data_root + 'FullDataSet/Annotations/',
-        # img_prefix=data_root + 'FullDataSet/AllImages/',
-        # pipeline=train_pipeline),
-    # val=dict(
-        # type=dataset_type,
-        # imgset=data_root + 'ImageSets/val.txt',
-        # ann_file=data_root + 'FullDataSet/Annotations/',
-        # img_prefix=data_root + 'FullDataSet/AllImages/',
-        # pipeline=test_pipeline),
-    # test=dict(
-        # type=dataset_type,
-        # imgset=data_root + 'ImageSets/val.txt',
-        # ann_file=data_root + 'FullDataSet/Annotations/',
-        # img_prefix=data_root + 'FullDataSet/AllImages/',
-        # pipeline=test_pipeline))
-# evaluation = None
+# default runtime
+checkpoint_config = dict(interval=3)
 
-# disable evluation, only need train and test
-# uncomments it when use trainval as train
-data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=4,
-    train=dict(
-        type=dataset_type,
-        imgset=data_root + 'ImageSets/trainval.txt',
-        ann_file=data_root + 'FullDataSet/Annotations/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
-        pipeline=train_pipeline),
-    test=dict(
-        type=dataset_type,
-        imgset=data_root + 'ImageSets/test.txt',
-        ann_file=data_root + 'FullDataSet/Annotations/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
-        pipeline=test_pipeline))
+# lr schedule
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+# RetinaNet nms is slow in early stage, disable every epoch evaluation
 evaluation = None
+
+model = dict(
+    type='RetinaNetOBB',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5),
+    bbox_head=dict(
+        type='OBBRetinaHead',
+        num_classes=1,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='Theta0AnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='OBB2OBBDeltaXYWHTCoder',
+            target_means=[.0, .0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)))
+# training and testing settings
+train_cfg = dict(
+    assigner=dict(
+        type='MaxIoUAssigner',
+        pos_iou_thr=0.5,
+        neg_iou_thr=0.4,
+        min_pos_iou=0,
+        gpu_assign_thr=-1,
+        ignore_iof_thr=-1,
+        iou_calculator=dict(
+            type='OBBOverlaps')),
+    allowed_border=-1,
+    pos_weight=-1,
+    debug=False)
+test_cfg = dict(
+    nms_pre=2000,
+    min_bbox_size=0,
+    score_thr=0.05,
+    nms=dict(type='obb_nms', iou_thr=0.1),
+    max_per_img=2000)
--- configs/obb/roi_transformer/faster_rcnn_roitrans_r50_fpn_3x_hrsc.py
+++ configs/obb/roi_transformer/faster_rcnn_roitrans_r50_fpn_3x_hrsc.py
@@ -1,87 +1,173 @@
-_base_ = './faster_rcnn_roitrans_r50_fpn_1x_dota10.py'
-
-# 3x schedules
-lr_config = dict(step=[24, 33])
-total_epochs = 36
-
-# HRSC2016 dataset cfg
-dataset_type = 'HRSCDataset'
-data_root = 'data/hrsc/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadOBBAnnotations', with_bbox=True,
-         with_label=True, with_poly_as_mask=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='OBBRandomFlip', h_flip_ratio=0.5, v_flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='RandomOBBRotate', rotate_after_flip=True,
-         angles=(0, 0), vert_rate=0.5),
-    dict(type='Pad', size_divisor=32),
-    dict(type='Mask2OBB', obb_type='obb'),
-    dict(type='OBBDefaultFormatBundle'),
-    dict(type='OBBCollect', keys=['img', 'gt_bboxes', 'gt_obboxes', 'gt_labels'])
-]
-test_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(
-        type='MultiScaleFlipRotateAug',
-        img_scale=[(1333, 800)],
-        h_flip=False,
-        v_flip=False,
-        rotate=False,
-        transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='OBBRandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='RandomOBBRotate', rotate_after_flip=True),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(type='OBBCollect', keys=['img']),
-        ])
+_base_ = [
+    '../_base_/datasets/hrsc.py',
+    '../_base_/schedules/schedule_3x.py',
+    '../../_base_/default_runtime.py'
 ]
 
-# does evaluation while training
-# uncomments it  when you need evaluate every epoch
-# data = dict(
-    # samples_per_gpu=2,
-    # workers_per_gpu=4,
-    # train=dict(
-        # type=dataset_type,
-        # imgset=data_root + 'ImageSets/train.txt',
-        # ann_file=data_root + 'FullDataSet/Annotations/',
-        # img_prefix=data_root + 'FullDataSet/AllImages/',
-        # pipeline=train_pipeline),
-    # val=dict(
-        # type=dataset_type,
-        # imgset=data_root + 'ImageSets/val.txt',
-        # ann_file=data_root + 'FullDataSet/Annotations/',
-        # img_prefix=data_root + 'FullDataSet/AllImages/',
-        # pipeline=test_pipeline),
-    # test=dict(
-        # type=dataset_type,
-        # imgset=data_root + 'ImageSets/val.txt',
-        # ann_file=data_root + 'FullDataSet/Annotations/',
-        # img_prefix=data_root + 'FullDataSet/AllImages/',
-        # pipeline=test_pipeline))
-# evaluation = dict(metric='mAP')
+# default runtime
+checkpoint_config = dict(interval=3)
 
-# disable evluation, only need train and test
-# uncomments it when use trainval as train
-data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=4,
-    train=dict(
-        type=dataset_type,
-        imgset=data_root + 'ImageSets/trainval.txt',
-        ann_file=data_root + 'FullDataSet/Annotations/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
-        pipeline=train_pipeline),
-    test=dict(
-        type=dataset_type,
-        imgset=data_root + 'ImageSets/test.txt',
-        ann_file=data_root + 'FullDataSet/Annotations/',
-        img_prefix=data_root + 'FullDataSet/AllImages/',
-        pipeline=test_pipeline))
-evaluation = None
+model = dict(
+    type='RoITransformer',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0 / 9.0, loss_weight=1.0)),
+    roi_head=dict(
+        type='RoITransRoIHead',
+        num_stages=2,
+        stage_loss_weights=[1, 1],
+        bbox_roi_extractor=[
+            dict(
+                type='SingleRoIExtractor',
+                roi_layer=dict(type='RoIAlign', out_size=7, sample_num=2),
+                out_channels=256,
+                featmap_strides=[4, 8, 16, 32]),
+            dict(
+                type='OBBSingleRoIExtractor',
+                roi_layer=dict(type='RoIAlignRotated', out_size=7, sample_num=2),
+                out_channels=256,
+                extend_factor=(1.4, 1.2),
+                featmap_strides=[4, 8, 16, 32]),
+        ],
+        bbox_head=[
+            dict(
+                type='OBBShared2FCBBoxHead',
+                start_bbox_type='hbb',
+                end_bbox_type='obb',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                bbox_coder=dict(
+                    type='HBB2OBBDeltaXYWHTCoder',
+                    target_means=[0., 0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2, 1]),
+                reg_class_agnostic=True,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0)),
+            dict(
+                type='OBBShared2FCBBoxHead',
+                start_bbox_type='obb',
+                end_bbox_type='obb',
+                in_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=1,
+                bbox_coder=dict(
+                    type='OBB2OBBDeltaXYWHTCoder',
+                    target_means=[0., 0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1, 0.5]),
+                reg_class_agnostic=False,
+                loss_cls=dict(
+                    type='CrossEntropyLoss',
+                    use_sigmoid=False,
+                    loss_weight=1.0),
+                loss_bbox=dict(type='SmoothL1Loss', beta=1.0,
+                               loss_weight=1.0))
+        ]))
+# model training and testing settings
+train_cfg = dict(
+    rpn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.7,
+            neg_iou_thr=0.3,
+            min_pos_iou=0.3,
+            match_low_quality=True,
+            gpu_assign_thr=200,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=256,
+            pos_fraction=0.5,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=False),
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False),
+    rpn_proposal=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=2000,
+        max_num=2000,
+        nms_thr=0.7,
+        min_bbox_size=0),
+    rcnn=[
+        dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                ignore_iof_thr=-1,
+                iou_calculator=dict(type='BboxOverlaps2D')),
+            sampler=dict(
+                type='RandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False),
+        dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=False,
+                ignore_iof_thr=-1,
+                iou_calculator=dict(type='OBBOverlaps')),
+            sampler=dict(
+                type='OBBRandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)])
+test_cfg = dict(
+    rpn=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=2000,
+        max_num=2000,
+        nms_thr=0.7,
+        min_bbox_size=0),
+    rcnn=dict(
+        score_thr=0.05,
+        score_weights=[0, 1],
+        nms=dict(type='obb_nms', iou_thr=0.1), max_per_img=2000))
