7ff5344dfab4b3795177494c1fbb91e27059f5d8
jbwang1997 <jbwang1997@gmail.com>
2021-08-27 21:43:40 +0800

    Add get_bbox_areas

--- mmdet/core/bbox/transforms_obb/__init__.py
+++ mmdet/core/bbox/transforms_obb/__init__.py
@@ -4,5 +4,5 @@ from .mapping import (hbb_flip, obb_flip, poly_flip, hbb_warp, obb_warp,
                       poly_warp, hbb_mapping, obb_mapping, poly_mapping,
                       hbb_mapping_back, obb_mapping_back, poly_mapping_back,
                       arb_mapping, arb_mapping_back)
-from .misc import (get_bbox_type, get_bbox_dim, choice_by_type, arb2result,
-                   arb2roi, regular_theta, regular_obb, mintheta_obb)
+from .misc import (get_bbox_type, get_bbox_dim, get_bbox_areas, choice_by_type,
+                   arb2result, arb2roi, regular_theta, regular_obb, mintheta_obb)
